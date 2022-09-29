7a5e38ffa83d2479f4f66b72be6a2e4d53789a6c
Brady Yanoff <113472249+Bradyyanoff@users.noreply.github.com>
2022-09-29 11:52:13 -0400

    Second window added

--- Shape.py
+++ Shape.py
@@ -6,10 +6,17 @@ arcade.set_background_color(arcade.color.RED)
 arcade.start_render()
 
 #arcade.draw_line_strip([(50, 50), (50, 300), (300, 50), (300, 300)], arcade.color.GOLD, 3)
-arcade.draw_lrtb_rectangle_filled(320, 420, 300, 50, arcade.color.GOLD)
-arcade.draw_lrtb_rectangle_outline(450, 550, 300, 50, arcade.color.GOLD, 12)
-arcade.draw_xywh_rectangle_filled(380, 120, 300, 80, arcade.color.AO)
-arcade.draw_xywh_rectangle_outline(10, 10, 680, 300, arcade.color.CARDINAL, 8)
+
+#arcade.draw_lrtb_rectangle_filled(320, 420, 300, 50, arcade.color.GOLD)
+#arcade.draw_lrtb_rectangle_outline(450, 550, 300, 50, arcade.color.GOLD, 12)
+#arcade.draw_xywh_rectangle_filled(380, 120, 300, 80, arcade.color.AO)
+#arcade.draw_xywh_rectangle_outline(10, 10, 680, 300, arcade.color.CARDINAL, 8)
+
+#arcade.draw_circle_filled(450, 300, 75, arcade.color.DARK_TAUPE)
+#arcade.draw_circle_outline(450, 300, 150, arcade.color.FLUORESCENT_YELLOW, 15)
+
+#arcade.draw_triangle_filled(300, 600, 600, 600, 450, 350, arcade.color.BLUE_SAPPHIRE)
+#arcade.draw_triangle_outline(600, 600, 490, 300, 700, 700, arcade.color.CADET_GREY, 15)
 
 
 
